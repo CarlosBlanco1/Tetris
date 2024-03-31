b9e788831d3a8259e7973c8415cc28b508fa0c04
CarlosBlanco1 <97683431+CarlosBlanco1@users.noreply.github.com>
2024-03-31 17:22:22 -0600

    First test Passing

--- Tetris.Console/Program.cs
+++ Tetris.Console/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
 Console.WriteLine("Hello, World!");
+
+public class Board()
+{
+    public static IEnumerable<List<string>>? CreateBoard(int columns, int rows)
+    {
+        return new List<List<string>>(){};
+    }
+}
--- Tetris.Tests/UnitTest1.cs
+++ Tetris.Tests/UnitTest1.cs
@@ -3,8 +3,14 @@ namespace Tetris.Tests;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void CanCreateTest()
     {
+        Assert.Equal(0, 0);
+    }
 
+    [Fact]
+    public void CanCreateBoard()
+    {
+        Assert.Equal(Board.CreateBoard(0, 0), new List<List<string>>(){});
     }
 }
