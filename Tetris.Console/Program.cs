f2515dfa18d363d62d79662d05930fed9e3ec617
CarlosBlanco1 <97683431+CarlosBlanco1@users.noreply.github.com>
2024-03-31 17:30:15 -0600

    Test 2 REFACTOR

--- Tetris.Console/Program.cs
+++ Tetris.Console/Program.cs
@@ -6,6 +6,10 @@ public class Board()
 {
     public static IEnumerable<List<string>>? CreateBoard(int columns, int rows)
     {
+        if(columns > 0)
+        {
+            return new List<List<string>>(){new List<string>()};
+        }
         return new List<List<string>>(){};
     }
 }
--- Tetris.Tests/UnitTest1.cs
+++ Tetris.Tests/UnitTest1.cs
@@ -17,7 +17,6 @@ public class UnitTest1
     [Fact]
     public void CanCreate1x1Board()
     {
-        Assert.Equal(Board.CreateBoard(1, 1), new List<List<string>>(){new List<string>(),
-        new List<string>()});
+        Assert.Equal(Board.CreateBoard(1, 1), new List<List<string>>(){new List<string>()});
     }
 }
