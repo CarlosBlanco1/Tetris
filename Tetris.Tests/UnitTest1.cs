a1bf187a1b57ce57969b465aaddab6f4843547e9
CarlosBlanco1 <97683431+CarlosBlanco1@users.noreply.github.com>
2024-03-31 17:25:45 -0600

    Second Test Red

--- Tetris.Tests/UnitTest1.cs
+++ Tetris.Tests/UnitTest1.cs
@@ -11,6 +11,13 @@ public class UnitTest1
     [Fact]
     public void CanCreateBoard()
     {
-        Assert.Equal(Board.CreateBoard(0, 0), new List<List<string>>(){});
+        Assert.Equal(Board.CreateBoard(0, 0), new List<List<string>>() { });
+    }
+
+    [Fact]
+    public void CanCreate1x1Board()
+    {
+        Assert.Equal(Board.CreateBoard(1, 1), new List<List<string>>(){new List<string>(),
+        new List<string>()});
     }
 }
